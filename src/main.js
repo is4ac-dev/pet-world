71710553239fdee08dddbdfaa368119435c415ea
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-08 23:56:11 -0300

    Aplicando Modo Estrito e importando CSS pelo Javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 // main.js => Enterpoint da aplicação
+
+// Modo Estrito => Mostra alertas silenciosos
+"use strict"
+
+// Importando CSS
+import "./styles/global.css"
+import "./styles/main.css"
+import "./styles/schedule.css"
+import "./styles/media.css"
