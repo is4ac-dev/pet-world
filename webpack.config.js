725a039e126ad57b70f20e690bf5aec28de769e1
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-08 23:36:41 -0300

    Aplicando configurações do Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,20 @@ module.exports = {
   target: "web",
   mode: "development",
 
+  // Configurações de input e output do Bundler
   entry: path.resolve(__dirname, "src", "main.js"), // Diretório do Enterpoint do Bundler
   output: {                                         // Configurações de saída do Bundler
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+
+  // Configurações do Dev Server
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
   }
 }
