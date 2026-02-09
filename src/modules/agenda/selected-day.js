7ea2516cd78aa111dfb270a8c85ac6db69600cd8
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-09 09:24:41 -0300

    Carregando data atual para input dentro do formulário de agendamento

--- src/modules/agenda/selected-day.js
+++ src/modules/agenda/selected-day.js
@@ -2,10 +2,14 @@
 import dayjs from "dayjs";
 
 // Recebendo o elemento de seletor de data da agenda
-const selectedDate = document.getElementById("agenda-date-selector")
+const selectedAgendaDate = document.getElementById("agenda-date-selector")
+
+// Recebendo o elemento de seletor de data para agendamento
+const selectedScheduleDate = document.getElementById("schedule-date")
 
 // Recebendo data atual
 const currentDate = dayjs().format("YYYY-MM-DD")
 
-// Carregando a data atual do atendimento e define a data mínima para atendimento
-selectedDate.value = selectedDate.min = currentDate
+// Carregando a data atual do atendimento e define a data mínima para atendimento e agendamento
+selectedAgendaDate.value = selectedAgendaDate.min = currentDate
+selectedScheduleDate.value = selectedScheduleDate.min = currentDate
