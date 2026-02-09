789e0098367da8e695c01ee0246a558e067e059f
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-09 20:07:13 -0300

    Criando script para renderizar horários disponíveis para agendamento com base na API

---
+++ src/modules/schedule/load-schedules.js
@@ -0,0 +1,27 @@
+// Importando módulo seletor de dia
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+
+// Importando módulo para verificar disponibilidade dos horários
+import { availableHours } from "../../utils/available-hours.js"
+
+// Importando módulo para construir opções dos horários
+import { buildAvailableHours } from "../../utils/build-available-hours.js"
+
+// Recebendo o elemento de seletor de data da agenda
+const dateAgendaSelector = document.getElementById("schedule-hour")
+
+// Esperando pela seleção de um dia
+dateAgendaSelector.addEventListener("input", async (event) => {
+  
+  // Recebendo dia selecionado
+  const date = event.target.value
+
+  // Requisitando agendamentos da API
+  const dailySchedules = await scheduleFetchByDay({ date })
+
+  // Verificando horários disponíveis a partir da data selecionada
+  const hours = availableHours({ date, dailySchedules })
+
+  // Construindo opções de horários
+  buildAvailableHours(hours)
+})
