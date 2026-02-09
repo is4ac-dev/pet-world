074c71df6723743d88fb5862ef1084060304d7b0
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-09 19:11:08 -0300

    Criando script padrão para exibir agendamentos do dia atual

---
+++ src/modules/agenda/default.js
@@ -0,0 +1,17 @@
+// Importando módulo seletor de dia
+import { scheduleFecthByDay } from "../../services/schedule-fetch-by-day.js"
+
+// Importando módulo para exibir agendamentos
+import { schedulesShow } from "../agenda/show.js"
+
+// Recebendo o elemento de seletor de data da agenda
+const dateAgendaSelector = document.getElementById("agenda-date-selector")
+
+// Recebendo dia selecionado
+const date = dateAgendaSelector.value
+
+// Requisitando agendamentos da API
+const dailySchedules = await scheduleFecthByDay({ date })
+
+// Exibindo os agendamentos na agenda
+schedulesShow({ dailySchedules })
