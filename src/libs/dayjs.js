6276491c3e576b670e6e29e8148087c33d9fdaa6
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-09 00:15:17 -0300

    Aplicando configurações de região/linguagem no DAYJS

---
+++ src/libs/dayjs.js
@@ -0,0 +1,6 @@
+// Importando módulo principal e linguagem a ser utilizada
+import dayjs from "dayjs"
+import "dayjs/locale/pt-br"
+
+// Configurando módulo para utilizar a linguagem PT-BR
+dayjs.locale("pt-br")
