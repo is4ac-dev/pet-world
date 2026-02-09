ea50c86a6ab52b075588a932c74fe894606e32e8
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-02-09 10:44:30 -0300

    Adicionando script para button de cancelar novo agendamento

--- src/modules/schedule/new-schedule-btn.js
+++ src/modules/schedule/new-schedule-btn.js
@@ -1,19 +1,34 @@
 // Importando módulo de Blur
-import { blurTrue } from "../../utils/blur.js"
+import { toggleBlur } from "../../utils/blur.js"
 
 // Recebendo botão de novo agendamento
 const newScheduleBtn = document.getElementById("new-schedule-btn")
 
+// Recebendo botão de cancelar novo agendamento
+const cancelNewSchedule = document.getElementById("cancel-button")
+
 // Recebendo sessão de novo agendamento
 const newScheduleSection = document.querySelector("dialog")
 
-// Esperando botão ser clicado
+// Esperando botão de registrar novo agendamento
 newScheduleBtn.onclick = () => {
   console.log("Registrando novo agendamento!")
 
   // Aplicando blur e desabilitando eventos de ponteiro
-  blurTrue()
+  toggleBlur(true)
 
   // Abrindo sessão de novo agendamento
   newScheduleSection.setAttribute("open", true)
+}
+
+// Esperando botão de cancelar
+cancelNewSchedule.onclick = () => {
+  console.log("Cancelando novo agendamento!")
+
+  // Removendo blur e habilitando eventos de ponteiro
+  toggleBlur(false)
+
+  // Fechando sessão de novo agendamento
+  newScheduleSection.removeAttribute("open")
+
 }
